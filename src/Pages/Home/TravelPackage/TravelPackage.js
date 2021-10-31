061aadf4610facfa3dfcd276ac42b038d9b6ef81
mdsajedul <sajedulislms@gmail.com>
2021-10-31 14:44:33 +0600

    package details half done

--- src/Pages/Home/TravelPackage/TravelPackage.js
+++ src/Pages/Home/TravelPackage/TravelPackage.js
@@ -12,11 +12,10 @@ const TravelPackage = ({travelPackage}) => {
             </div>
             <div className="d-flex justify-content-between content-conteiner">
                 <p>${cost}/Per Person</p>
-                <p><i class="far fa-calendar-alt"></i> {duration} Days</p>
-                
+                <p><i className="far fa-calendar-alt"></i> {duration} Days</p>
             </div>
             <div className="btn-conteiner">
-                <h4 className="location"><i class="fas fa-map-marker-alt "></i> {name}</h4>
+                <h4 className="location"><i className="fas fa-map-marker-alt "></i> {name}</h4>
                 <Link to={`/packagedetail/${_id}`}>
                     <button className="btn-book">Book Now</button>
                 </Link>
--- src/Pages/PackageDetails/PackageDetails.js
+++ src/Pages/PackageDetails/PackageDetails.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import "./packageDetails.css";
 
 const PackageDetails = () => {
     const {packageId} = useParams();
+    const [travelPackage,setTravelPackage]= useState({});
+    useEffect(()=>{
+        fetch(`http://localhost:5000/packages/${packageId}`)
+        .then(res=>res.json())
+        .then(data=>setTravelPackage(data));
+    },[])
+    console.log(travelPackage)
+    
     return (
         <div>
-            <h2>Details {packageId}</h2>
+            <div className="package-banner "/>
+            <div className="package-content d-flex justify-content-center align-items-center">
+                <h1>Tour Package Details</h1>
+            </div>
+            <div className="package-details">
+                <div className="row gx-0 m-2">
+                    <div className="col-lg-8 p-5">
+                        <img className="package-img" src={travelPackage.image} alt="" />
+                        <h1 className="my-3">{travelPackage.name}</h1>
+                        < hr className="hr-color" />
+                        <div className="d-flex justify-content-between content-conteiner">
+                            <p>${travelPackage.cost}/Per Person</p>
+                            <p><i className="far fa-calendar-alt"></i> {travelPackage.duration} Days</p>
+                        </div>
+                        < hr className="hr-color" />
+                        <div>
+                            <h4>Overview</h4>
+                            <p>{travelPackage.overview}</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 ">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
