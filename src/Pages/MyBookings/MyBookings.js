f83766fd6fbf72d290f0223235c7c86fb7113c4c
mdsajedul <sajedulislms@gmail.com>
2021-11-01 00:44:55 +0600

    my bookings items show and delete btn added

--- src/Pages/MyBookings/MyBookings.js
+++ src/Pages/MyBookings/MyBookings.js
@@ -1,17 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import useAuth from '../../Hooks/useAuth';
 import './myBookings.css';
 
 const MyBookings = () => {
     const [bookings,setBookings] = useState([])
+    const [count,setCount] = useState(0);
+    const {user} = useAuth()
     useEffect(()=>{
-
+        fetch(`http://localhost:5000/bookings/${user?.email}`)
+        .then(res=>res.json())
+        .then(data=>setBookings(data))
     },[])
+
+    const deleteHandler=()=>{
+        
+    }
+
     return (
         <div className="p-5">
             <div className="my-bookings-container w-100">
-                <div className="bookings-header d-flex align-items-center p-2">
-                    <i class="fas fa-list pe-2"></i> 
-                    <h4>My Bookings List</h4>
+                <div className="bookings-header  p-2">
+                    <div className="d-flex align-items-center">
+                        <i class="fas fa-list pe-2"></i> 
+                        <h4>My Bookings List</h4>
+                    </div>
+                </div>
+                <div>
+                    <ul>
+                    {
+                        bookings.map(booking=> <li className="booking-content my-3">
+                            <div className="d-flex justify-content-between">
+                                <div className="d-flex align-items-center ">
+                                    <i class="fas fa-chevron-right pe-2"></i>
+                                    <h5>{booking.packageName}</h5>
+                                </div>
+                                <div>
+                                    <button onClick={deleteHandler} className="delete-btn"><i class="fas fa-trash-alt"></i></button>
+                                </div>
+                            </div>
+                            
+                            <hr />
+                        </li>)
+                    }
+                    </ul>
+                    
                 </div>
             </div>
         </div>
